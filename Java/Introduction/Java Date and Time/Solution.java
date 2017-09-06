ab63f5a92983cc8e02118809511b9fdc791c906d
Infiltrator <rupesh.jackson@gmail.com>
2017-09-06 12:16:07 +0530

    Update Solution.java
    Updated as per the new requirement in HackerRank.

--- Java/Introduction/Java Date and Time/Solution.java
+++ Java/Introduction/Java Date and Time/Solution.java
@@ -2,20 +2,26 @@
 //     Github: github.com/rshaghoulian
 // HackerRank: hackerrank.com/rshaghoulian
 
-import java.util.Scanner;
 import java.time.LocalDate;
+import java.util.*;
+import java.text.*;
 
 public class Solution {
-    public static void main(String[] args) {
-        /* Read input */
-        Scanner scan = new Scanner(System.in);
-        int month = scan.nextInt();
-        int day   = scan.nextInt();
-        int year  = scan.nextInt();
-        scan.close();
-        
-        /* Create LocalDate instance */
-        LocalDate date = LocalDate.of(year, month, day);
-        System.out.println(date.getDayOfWeek());
+    
+    public static String getDay(String day, String month, String year) {
+        try{
+            Date date1=new SimpleDateFormat("dd MM yyyy").parse(day+" "+month+" "+year);
+            String ret = new SimpleDateFormat("EEEE").format(date1);
+            return ret.toUpperCase();
+            }catch(Exception e){}
+           return "";
+    }
+    
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();   
+        System.out.println(getDay(day, month, year));
     }
 }
